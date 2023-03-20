fd473db8a9ec7c28ecb6cae698fc1d64ad8bdc29
Umesh Satam <ninad.umesh-satam@capgemini.com>
2023-03-20 15:23:01 +0530

    removed puppeteer-core

--- automation.js
+++ automation.js
@@ -9,7 +9,6 @@ const automation = async (res) => {
       "--single-process",
       "--no-zygote",
     ],
-    headless: true,
     executablePath:
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
